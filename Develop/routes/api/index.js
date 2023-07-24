c804822bb439a63777b4bd565db515458a3335ad
zoe <zoeedge0703@gmail.com>
2023-07-23 20:17:31 -0500

    changed the paths in the index.js under api folder

--- Develop/routes/api/index.js
+++ Develop/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const categoryRoutes = require('./category');
+const productRoutes = require('./product');
+const tagRoutes = require('./tag');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
